f6cc38fbbf07182bc2f582bc4aca290290392b2e
Abishek555k <abishekpriya091@gmail.com>
2024-09-28 19:56:00 +0530

    Add files via upload

---
+++ intex.js
@@ -0,0 +1,37 @@
+var sidenav = document.querySelector(".side-navbar");
+          
+function showside()
+{
+sidenav.style.left = "0%";
+}
+ function closenav(){
+  sidenav.style.left = "-60%";
+ }
+    
+ var productcontainer = document.getElementById("products")
+ var search = document.getElementById("search")
+ 
+ var productlist = productContainer.querySelectorAll("div")
+ 
+ search.addEventListener("keyup", function(){
+ 
+   var enteredValue = event.target.value.toUpperCase()
+ 
+   for(count=0;count<productlist.length;count=count+1)
+  {
+ 
+ 
+ 
+      var productname = productlist[count].querySelector("p").textContent
+ 
+     if(productname.toUpperCase().indexOf(enteredValue)<0)
+     {
+     productlist[count].style.display="none"
+     }
+ 
+     else{
+     productlist[count].style.display="block"
+     }
+    }
+   })
+ 
